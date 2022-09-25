ebb3d74f82424aee0598a4db7d6b13381d86abbb
Jay Shankar Sharma <jayshankar.s@wisflux.com>
2022-09-25 17:59:36 +0530

    3

--- infra/scritps/test.sh
+++ infra/scritps/test.sh
@@ -8,14 +8,19 @@ echo "Hello branch $1 "
 echo "Hello branch $2 "
 echo "Commit message $3"
 
-
+# Make changes in main branch
 git add .
 git commit -m "$3"
 git push
 
-git checkout "$2"
+git pull
+git rebase "refs/heads/$2"
+git checkout "refs/heads/$2"
+git pull
 git merge "$1"
 git push 
+git checkout "$1"
+git pull
 
 # git rebase "$2"
 # git commit -m "$3"
